c774e469d90a81df606ea5ddac2296eab5362819
amaki <in345vain@gmail.com>
2021-12-12 09:46:32 +0900

    initial CRUD & db done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,36 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    if task.save
+      redirect_to tasks_path, notice: "タスク「#{task.name}」を登録しました"
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to tasks_path, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
+
+  private
+    def task_params
+      params.require(:task).permit(:name,:description)
+    end
+end
