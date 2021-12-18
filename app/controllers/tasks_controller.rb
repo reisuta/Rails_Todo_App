607113f726061c5363ee88056dfb0ae0b303e9f8
amaki <in345vain@gmail.com>
2021-12-18 11:44:07 +0900

    modified task.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(task_params)
-    if task.save
-      redirect_to tasks_path, notice: "タスク「#{task.name}」を登録しました"
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました"
+    else
+      render :new
     end
   end
 
